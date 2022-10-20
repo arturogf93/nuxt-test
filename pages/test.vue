5c37bff64a833b825986be59be9592b4d6d060a7
Arturo Armando González Fernández <arturo1_g@hotmail.com>
2022-10-20 11:56:32 -0500

    meta test

---
+++ pages/test.vue
@@ -0,0 +1,19 @@
+<script setup>
+const { data: pokemon } = await useFetch('https://pokeapi.co/api/v2/pokemon/ditto')
+</script>
+<template>
+  <div>
+
+    <Head>
+      <Title>{{ pokemon.name }}</Title>
+      <Meta name="description" :content="pokemon.name" />
+      <Meta property="og:title" content="pokemon.name" />
+      <Meta property="og:image" content="pokemon.sprites.front_default" />
+    </Head>
+
+    {{ pokemon.name }}
+    <div>
+      <img :src="pokemon.sprites.front_default" alt="">
+    </div>
+  </div>
+</template>
